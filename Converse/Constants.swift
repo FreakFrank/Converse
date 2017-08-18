b7bd086849da1bd30154cdaf6a77bfb8a2d17865
FreakFrank <kareem.ismailmohamed@gmail.com>
2017-08-18 23:05:30 +0200

    Done with login VC UI

---
+++ Converse/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Converse
+//
+//  Created by Kareem Ismail on 8/18/17.
+//  Copyright © 2017 Whatever. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
---
+++ Converse/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Converse
+//
+//  Created by Kareem Ismail on 8/18/17.
+//  Copyright © 2017 Whatever. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
