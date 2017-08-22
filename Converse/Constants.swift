4ace27b9c109851e1ba660399754b8480f6173fc
FreakFrank <kareem.ismailmohamed@gmail.com>
2017-08-22 17:14:05 +0200

    done with registering users

--- Converse/Constants.swift
+++ Converse/Constants.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URLS
+
+let BASE_URL = "https://conversea.herokuapp.com/v1/"
+let REGISTER_URL = "\(BASE_URL)account/register"
+
 // Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "toChannel"
+
+// User defaults 
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
--- Converse/CreateAccountViewController.swift
+++ Converse/CreateAccountViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,4 +23,25 @@ class CreateAccountViewController: UIViewController {
         performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
     }
 
+    @IBAction func createAccBtnPressed(_ sender: Any) {
+        
+        guard let email = emailTextField.text, emailTextField.text != "" else {
+            return
+        }
+        guard let password = passwordTextField.text, passwordTextField.text != "" else {
+            return
+        }
+        
+        AuthService.instance.registerUser(email: email, password: password){
+            (success) in
+            if success {
+                print("Registerd user successfully")
+            }
+        }
+        
+    }
+    @IBAction func chooseAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func generateBckGrndPressed(_ sender: Any) {
+    }
 }
